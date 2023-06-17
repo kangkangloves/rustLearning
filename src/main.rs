4dfdd3b8ce2cacc43d70dc3ad01c2c283bc85775
KangKang <131173673+kangkangloves@users.noreply.github.com>
2023-06-17 05:10:52 +0000

    feat: 添加HTTP服务器和路由 在main.rs中，添加了一个HTTP服务器和路由，当请求路径为/时，返回一个包含“Hello, World!”文本的响应。使用warp库中的Filter和Reply trait实现路由，并使用tokio宏标记main函数，以便它可以在异步上下文中运行。
    该提交包含以下更改：
    
    添加main.rs文件
    在main.rs中添加HTTP服务器和路由

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #![allow(non_snake_case)]
-fn main() {
-    println!("Hello, world!");
-    // input some number
-    let mut input = String::new();
-    // input
-    std::io::stdin().read_line(&mut input).unwrap();
-    
+// 导入warp库中的Filter和Reply trait
+use warp::{Filter, Reply};
+
+// 使用tokio宏标记main函数，以便它可以在异步上下文中运行
+#[tokio::main]
+async fn main() {
+    // 创建一个新的路由，当请求路径为/时，返回一个包含“Hello, World!”文本的响应
+    let routes = warp::path::end().map(|| "Hello, World!");
+
+    // 启动一个新的HTTP服务器，监听本地主机的端口3000，并运行路由
+    warp::serve(routes).run(([127, 0, 0, 1], 3000)).await;
 }
