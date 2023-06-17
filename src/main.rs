5776f24dd9d2578d2650a2ca534c6eed44af9c3d
KangKang <131173673+kangkangloves@users.noreply.github.com>
2023-06-17 04:29:58 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#![allow(non_snake_case)]
+fn main() {
+    println!("Hello, world!");
+    // input some number
+    let mut input = String::new();
+    // input
+    std::io::stdin().read_line(&mut input).unwrap();
+    
+}
